ebe05fe92a2d822b02694f47162a0aa0fbd874ac
vunamweb <vukynamkhtn@gmail.com>
2023-12-25 11:24:58 +0700

    upload cache

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 // Version
-define('VERSION', '3.0.3.8');
+define('VERSION', '3.0.3.9');
 
 // Configuration
 if (is_file('config.php')) {
