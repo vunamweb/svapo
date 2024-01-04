9ebf8eb70fd060caa82f81b880b0d126dad66e30
vunamweb <vukynamkhtn@gmail.com>
2024-01-04 11:29:16 +0700

    category
    show atribute on frontend

--- catalog/controller/extension/module/category.php
+++ catalog/controller/extension/module/category.php
@@ -39,8 +39,15 @@ class ControllerExtensionModuleCategory extends Controller {
                 foreach ( $children as $child ) {
                     $filter_data = array( 'filter_category_id' => $child[ 'category_id' ], 'filter_sub_category' => true );
 
-                    $name = $child[ 'name' ];
-                    $name .= $this->model_catalog_category->getCategoryAttribute( $child[ 'category_id' ] );
+					$name = '<div class="item">';
+
+					$name .= '<h4 class="title text1 text-black font_Inter mb-lg-4 mb-2">' . $child[ 'name' ] . '</h4>';
+
+					$name .= $this->model_catalog_category->getCategoryAttribute( $child[ 'category_id' ] );
+					
+					$name .= '</div>';
+
+					//$name = 'ndd';
 
                     //echo $this->model_catalog_category->getCategoryAttribute( $child[ 'category_id' ] );
 
