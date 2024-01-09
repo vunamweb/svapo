0abff522c8b3ee8a3f1268366a83fdcd37ac6855
vunamweb <vukynamkhtn@gmail.com>
2024-01-09 23:52:57 +0700

    get file from server

--- cms/nogo/navarray_de.php
+++ cms/nogo/navarray_de.php
@@ -1,5 +1,5 @@
 <?php
-$navarray = array("0"=>"", "2"=>"ueber-uns", "4"=>"presse", "6"=>"kontakt", "7"=>"impressum", "8"=>"datenschutz", "20"=>"jobs", "21"=>"partner", "23"=>"hofladen", "24"=>"apfelsortenlexikon", "25"=>"obstsortenbestand", "26"=>"reservieren", "27"=>"oeffnungszeiten-hofladen", "31"=>"wissenwertes", "32"=>"was-ist-apfelwein", "33"=>"wie-trinkt-man-apfelwein", "39"=>"footer-spalte-1");
+$navarray = array("0"=>"", "2"=>"hometest");
 ?><?php
-$navarrayFULL = array("0"=>"", "2"=>"Über uns", "4"=>"Presse", "6"=>"Kontakt", "7"=>"Impressum", "8"=>"Datenschutz", "20"=>"Jobs", "21"=>"Partner", "23"=>"Hofladen", "24"=>"Apfelsortenlexikon", "25"=>"Obstsortenbestand", "26"=>"Reservieren", "27"=>"Öffnungszeiten Hofladen", "31"=>"Wissenwertes", "32"=>"Was ist Apfelwein", "33"=>"Wie trinkt man Apfelwein?", "39"=>"Footer Spalte 1");
+$navarrayFULL = array("0"=>"", "2"=>"hometest");
 ?>
