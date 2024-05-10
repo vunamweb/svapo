974e47cd6e5b02e56a1c57763930a2080e8567e1
vunamweb <vukynamkhtn@gmail.com>
2024-05-10 21:09:02 +0700

    used SMTP for forgot password

--- catalog/controller/mail/forgotten.php
+++ catalog/controller/mail/forgotten.php
@@ -1,4 +1,10 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+
+require "PHPMailer.php";
+require "SMTP.php";
+require "Exception.php";
+
 class ControllerMailForgotten extends Controller {
 	public function index(&$route, &$args, &$output) {			            
 		$this->load->language('mail/forgotten');
@@ -10,7 +16,7 @@ class ControllerMailForgotten extends Controller {
 		$data['reset'] = str_replace('&amp;', '&', $this->url->link('account/reset', 'code=' . $args[1], true));
 		$data['ip'] = $this->request->server['REMOTE_ADDR'];
 		
-		$mail = new Mail($this->config->get('config_mail_engine'));
+		/*$mail = new Mail($this->config->get('config_mail_engine'));
 		$mail->parameter = $this->config->get('config_mail_parameter');
 		$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
 		$mail->smtp_username = $this->config->get('config_mail_smtp_username');
@@ -23,6 +29,38 @@ class ControllerMailForgotten extends Controller {
 		$mail->setSender(html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8'));
 		$mail->setSubject(html_entity_decode(sprintf($this->language->get('text_subject'), html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8')), ENT_QUOTES, 'UTF-8'));
 		$mail->setText($this->load->view('mail/forgotten', $data));
-		$mail->send();
+		$mail->send();*/
+
+		$subject = html_entity_decode(sprintf($this->language->get('text_subject'), html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8')), ENT_QUOTES, 'UTF-8');
+
+		$this->sendMailSMTP($args[0], $subject, 'test@7sc.eu', $this->config->get('config_email'), $this->load->view('mail/forgotten', $data), $this->session->data['upload_file']);
+			
 	}
+
+	function sendMailSMTP($to, $subject, $from, $fromName, $message, $image = null)
+    {
+		$mail = new PHPMailer();
+		$mail->IsSMTP(); // telling the class to use SMTP
+		$mail->SMTPDebug = 0; // enables SMTP debug information (for testing)
+		$mail->SMTPAuth = true; // enable SMTP authentication
+		$mail->SMTPSecure = "ssl"; // sets the prefix to the servier
+		$mail->Host = SMTP_HOST; // sets GMAIL as the SMTP server
+		$mail->Port = 465; // set the SMTP port for the GMAIL server
+		$mail->Username = SMTP_USER; // GMAIL username
+		$mail->Password = SMTP_PASSWORD;
+		$mail->CharSet = 'UTF-8';
+		$mail->AddAddress($to);
+		//$mail->addBcc("vukynamkhtn@gmail.com");
+		$mail->Subject = $subject;
+		$mail->FromName = $fromName;
+		$mail->From = $from;
+		$mail->IsHTML(true);
+		$mail->Body = $message;
+
+		if (!$mail->Send()) {
+			//echo "Mailer Error: " . $mail->ErrorInfo;
+		} else {
+			//echo "Message sent!";
+		}
+    }
 }
