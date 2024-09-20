b52b4d1d8b6779997f774f236585b7e7933a8e9f
vunam <vukynamkhtn@gmail.com>
2024-09-20 14:26:12 +0700

    get index from server

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '0');
+error_reporting(0);
 // Version
+// phpinfo();
 define('VERSION', '3.0.3.9');
 
 // Configuration
@@ -10,6 +11,7 @@ if (is_file('config.php')) {
 	require_once('config.php');
 }
 
+// print_r($_COOKIE);
 // Install
 if (!defined('DIR_APPLICATION')) {
 	header('Location: install/index.php');
