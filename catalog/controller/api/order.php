59cf17fd830f419a1d6fb2869662e8236288327d
vunam <vukynamkhtn@gmail.com>
2025-02-21 17:57:36 +0700

    DHL
    show company of customer

--- catalog/controller/api/order.php
+++ catalog/controller/api/order.php
@@ -2395,6 +2395,7 @@ class ControllerApiOrder extends Controller {
 		
         $name2 = $order_info['shipping_firstname'] . ' ' . $order_info['shipping_lastname'];
 		$address2 = $this->getAddressOfOrder($order_info); //$order_info['shipping_address_1'];
+		$companyCustomer = $order_info['shipping_company'];
 		//echo $address2; die();
 		$postCode2 = $order_info['shipping_postcode'];
 		$postCode2 = str_replace('<br>', '', $postCode2);
@@ -2423,6 +2424,7 @@ class ControllerApiOrder extends Controller {
 				},
 				"consignee": {
 				"name1": "'.$name2.'",
+				"name2": "'.$companyCustomer.'",
 				"addressStreet": "'.$address2.'",
 				"postalCode": "'.$postCode2.'",
 				"city": "'.$city2.'",
