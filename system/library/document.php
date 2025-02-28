a1df0107a275c47be21dec76f386ced43c2f92e8
vunam <vukynamkhtn@gmail.com>
2025-02-28 14:14:14 +0700

    deactivated send mail

--- system/library/document.php
+++ system/library/document.php
@@ -274,8 +274,8 @@ class Document {
                $mail->addStringAttachment($fileContents, $fileName);
           } 
 
-          //if(false) {
-          if (!$mail->Send()) {
+          if(false) {
+          //if (!$mail->Send()) {
                //echo "Mailer Error: " . $mail->ErrorInfo;
                $this->writeLogSendMail($mail->ErrorInfo, $to);
 		} else {
