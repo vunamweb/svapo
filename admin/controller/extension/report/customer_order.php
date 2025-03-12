e689a02f490b265d47abc2fe784123614533acc7
vunam <vukynamkhtn@gmail.com>
2025-03-12 17:14:57 +0700

    report in admin_1
    get list of customer

--- admin/controller/extension/report/customer_order.php
+++ admin/controller/extension/report/customer_order.php
@@ -96,11 +96,36 @@ class ControllerExtensionReportCustomerOrder extends Controller {
 			$filter_order_status_id = 0;
 		}
 
+		if (isset($this->request->get['filter_gram_from'])) {
+			$filter_gram_from = $this->request->get['filter_gram_from'];
+		} else {
+			$filter_gram_from = '';
+		}
+
+		if (isset($this->request->get['filter_gram_to'])) {
+			$filter_gram_to = $this->request->get['filter_gram_to'];
+		} else {
+			$filter_gram_to = '';
+		}
+
+		if (isset($this->request->get['filter_price_from'])) {
+			$filter_price_from = $this->request->get['filter_price_from'];
+		} else {
+			$filter_price_from = '';
+		}
+
+		if (isset($this->request->get['filter_price_to'])) {
+			$filter_price_to = $this->request->get['filter_price_to'];
+		} else {
+			$filter_price_to = '';
+		}
+
 		if (isset($this->request->get['page'])) {
 			$page = (int)$this->request->get['page'];
 		} else {
 			$page = 1;
 		}
+		
 
 		$this->load->model('extension/report/customer');
 
@@ -110,14 +135,18 @@ class ControllerExtensionReportCustomerOrder extends Controller {
 			'filter_date_start'			=> $filter_date_start,
 			'filter_date_end'			=> $filter_date_end,
 			'filter_customer'			=> $filter_customer,
+			'filter_gram_from'			=> $filter_gram_from,
+			'filter_gram_to'			=> $filter_gram_to,
+			'filter_price_from'			=> $filter_price_from,
+			'filter_price_to'			=> $filter_price_to,
 			'filter_order_status_id'	=> $filter_order_status_id,
 			'start'						=> ($page - 1) * $this->config->get('config_limit_admin'),
 			'limit'						=> $this->config->get('config_limit_admin')
 		);
 
-		$customer_total = $this->model_extension_report_customer->getTotalOrders($filter_data);
-
-		$results = $this->model_extension_report_customer->getOrders($filter_data);
+		$customer_total = count($this->model_extension_report_customer->getOrders_adjust($filter_data)); //$this->model_extension_report_customer->getTotalOrders($filter_data);
+        //echo $customer_total;
+		$results = $this->model_extension_report_customer->getOrders_adjust($filter_data);
 
 		foreach ($results as $result) {
 			$data['customers'][] = array(
@@ -170,6 +199,11 @@ class ControllerExtensionReportCustomerOrder extends Controller {
 		$data['filter_date_end'] = $filter_date_end;
 		$data['filter_customer'] = $filter_customer;
 		$data['filter_order_status_id'] = $filter_order_status_id;
+		$data['filter_gram_from'] = $filter_gram_from;
+		$data['filter_gram_to'] = $filter_gram_to;
+		$data['filter_price_from'] = $filter_price_from;
+		$data['filter_price_to'] = $filter_price_to;
+		
 
 		return $this->load->view('extension/report/customer_order_info', $data);
 	}
--- admin/model/extension/report/customer.php
+++ admin/model/extension/report/customer.php
@@ -136,6 +136,62 @@ class ModelExtensionReportCustomer extends Model {
 		return $query->rows;
 	}
 
+	public function getOrders_adjust($data = array()) {
+		$filter_gram_from = (!empty($data['filter_gram_from'])) ? $data['filter_gram_from'] : 0;
+		$filter_gram_to = (!empty($data['filter_gram_to'])) ? $data['filter_gram_to'] : 20000000;
+		$filter_price_from = (!empty($data['filter_price_from'])) ? $data['filter_price_from'] : 0;
+		$filter_price_to = (!empty($data['filter_price_to'])) ? $data['filter_price_to'] : 0;
+		
+		$sql = "SELECT c.customer_id, CONCAT(c.firstname, ' ', c.lastname) AS customer, c.email, cgd.name AS customer_group, c.status, o.order_id, SUM(op.quantity) as products, o.total AS total FROM `" . DB_PREFIX . "order` o LEFT JOIN `" . DB_PREFIX . "order_product` op ON (o.order_id = op.order_id) LEFT JOIN `" . DB_PREFIX . "customer` c ON (o.customer_id = c.customer_id) LEFT JOIN `" . DB_PREFIX . "customer_group_description` cgd ON (c.customer_group_id = cgd.customer_group_id) WHERE o.customer_id > 0 AND cgd.language_id = '" . (int)$this->config->get('config_language_id') . "'";
+
+		if (!empty($data['filter_date_start'])) {
+			$sql .= " AND DATE(o.date_added) >= DATE('" . $this->db->escape($data['filter_date_start']) . "')";
+		}
+
+		if (!empty($data['filter_date_end'])) {
+			$sql .= " AND DATE(o.date_added) <= DATE('" . $this->db->escape($data['filter_date_end']) . "')";
+		}
+
+		if (!empty($data['filter_customer'])) {
+			$sql .= " AND CONCAT(c.firstname, ' ', c.lastname) LIKE '" . $this->db->escape($data['filter_customer']) . "'";
+		}
+
+		if (!empty($data['filter_order_status_id'])) {
+			$sql .= " AND o.order_status_id = '" . (int)$data['filter_order_status_id'] . "'";
+		} else {
+			$sql .= " AND o.order_status_id > '0'";
+		}
+
+		$sql .= " GROUP BY o.order_id";
+
+		if(empty($data['filter_gram_to']) && empty($data['filter_price_to']))
+		  $sql = "select * from (SELECT t.customer_id, t.customer, t.email, t.customer_group, t.status, COUNT(DISTINCT t.order_id) AS orders, SUM(t.products) AS products, SUM(t.total) AS total FROM (" . $sql . ") AS t GROUP BY t.customer_id ORDER BY total DESC) as t where products >= ".$filter_gram_from." AND total >= ".$filter_price_from."";
+		else if(empty($data['filter_gram_to']))
+		  $sql = "select * from (SELECT t.customer_id, t.customer, t.email, t.customer_group, t.status, COUNT(DISTINCT t.order_id) AS orders, SUM(t.products) AS products, SUM(t.total) AS total FROM (" . $sql . ") AS t GROUP BY t.customer_id ORDER BY total DESC) as t where products >= ".$filter_gram_from." AND total >= ".$filter_price_from." AND total <= ".$filter_price_to."";
+		else if(empty($data['filter_price_to']))
+		  $sql = "select * from (SELECT t.customer_id, t.customer, t.email, t.customer_group, t.status, COUNT(DISTINCT t.order_id) AS orders, SUM(t.products) AS products, SUM(t.total) AS total FROM (" . $sql . ") AS t GROUP BY t.customer_id ORDER BY total DESC) as t where products >= ".$filter_gram_from." AND total >= ".$filter_price_from." AND products <= ".$filter_gram_to."";
+		else 
+		  $sql = "select * from (SELECT t.customer_id, t.customer, t.email, t.customer_group, t.status, COUNT(DISTINCT t.order_id) AS orders, SUM(t.products) AS products, SUM(t.total) AS total FROM (" . $sql . ") AS t GROUP BY t.customer_id ORDER BY total DESC) as t where products >= ".$filter_gram_from." AND total >= ".$filter_price_from." AND products <= ".$filter_gram_to." AND total <= ".$filter_price_to."";
+		  
+		if (isset($data['start']) || isset($data['limit'])) {
+			if ($data['start'] < 0) {
+				$data['start'] = 0;
+			}
+
+			if ($data['limit'] < 1) {
+				$data['limit'] = 20;
+			}
+
+			$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
+		}
+
+		//echo $sql;
+		$query = $this->db->query($sql);
+
+		return $query->rows;
+	}
+
+
 	public function getTotalOrders($data = array()) {
 		$sql = "SELECT COUNT(DISTINCT o.customer_id) AS total FROM `" . DB_PREFIX . "order` o LEFT JOIN `" . DB_PREFIX . "customer` c ON (o.customer_id = c.customer_id) WHERE o.customer_id > '0'";
 
