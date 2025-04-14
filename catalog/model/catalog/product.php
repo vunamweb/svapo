4232b96b1dd5116141f81e4238d1e69e4246cb6c
vunam <vukynamkhtn@gmail.com>
2025-04-14 18:43:46 +0700

    get model of product from testshop

--- catalog/model/catalog/product.php
+++ catalog/model/catalog/product.php
@@ -464,7 +464,8 @@ class ModelCatalogProduct extends Model {
             'p.price',
             'rating',
             'p.sort_order',
-            'p.date_added'
+            'p.date_added',
+			'p.thc'
         );
 
         if ( isset( $data[ 'sort' ] ) && in_array( $data[ 'sort' ], $sort_data ) ) {
